89c1e715cb5e5c09a18c50427550516b073d7bfd
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-28 23:56:17 -0400

    Test

--- SistemaDeporteApi/Program.cs
+++ SistemaDeporteApi/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddEnvironmentVariables();
+
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
 builder.Services.AddDbContext<Contexto>(options =>
